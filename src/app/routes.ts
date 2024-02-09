af097395c58ef57b18ca6983514df4f4e965ab2c
C00P3R14 <ngubane.linda9@gmail.com>
2024-02-09 13:56:48 +0200

    Implement DashboardComponent

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from "@angular/router";
 import { HeroesComponent } from "./heroes/heroes.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
 
 
 export const routes:Routes = [
+    {path:'dashboard', component:DashboardComponent},
     {path:'heroes', component:HeroesComponent}
 ]
