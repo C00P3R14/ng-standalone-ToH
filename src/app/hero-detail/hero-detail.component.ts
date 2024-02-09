a3318a8272cc48a67569b65445f48a0165d589da
C00P3R14 <ngubane.linda9@gmail.com>
2024-02-09 12:43:37 +0200

    HeroDetailComponent

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input} from '@angular/core';
+import { Hero } from '../hero';
+import { FormsModule } from '@angular/forms';
+import { NgIf, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-hero-detail',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,UpperCasePipe,NgIf],
   templateUrl: './hero-detail.component.html',
   styleUrl: './hero-detail.component.css'
 })
 export class HeroDetailComponent {
-
+@Input() hero?:Hero
 }
